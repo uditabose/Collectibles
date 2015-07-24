36b92a8a4504a37642f29a1e719a3d211bb9b95d
uditabose <ub298@nyu.edu>
2015-07-24 13:14:41 -0400

    new practice

---
+++ Personal/HackerRank/src/quick/QuickCode.java
@@ -0,0 +1,15 @@
+
+
+package quick;
+
+/**
+ *
+ * @author Udita
+ */
+public class QuickCode {
+    
+    public static void main(String[] args) {
+        System.out.println( ('Z' - 'A'));
+    }
+
+}
