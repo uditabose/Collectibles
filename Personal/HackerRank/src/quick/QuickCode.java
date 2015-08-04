10099aff2822c139c98e85a37838b7a11613f1b7
uditabose <ub298@nyu.edu>
2015-08-03 23:00:50 -0400

    more practice

--- Personal/HackerRank/src/quick/QuickCode.java
+++ Personal/HackerRank/src/quick/QuickCode.java
@@ -10,6 +10,11 @@ public class QuickCode {
     
     public static void main(String[] args) {
         System.out.println( ('Z' - 'A'));
+        System.out.println(10 << 1);
+        System.out.println(10 >> 1);
+        System.out.println(20 ^ 20);
+        System.out.println(25 ^ 20);
+        System.out.println(20 ^ 25 ^ 20);
     }
 
 }
