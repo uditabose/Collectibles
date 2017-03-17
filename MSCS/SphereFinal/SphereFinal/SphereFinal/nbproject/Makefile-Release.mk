3af1f2a81d2ee0b2ad72e8ab7569ca5f2e1f3d33
Udita Bose <ubose@us.ibm.com>
2017-03-17 12:37:15 -0400

    Pratiquer moved

--- MSCS/SphereFinal/SphereFinal/SphereFinal/nbproject/Makefile-Release.mk
+++ MSCS/SphereFinal/SphereFinal/SphereFinal/nbproject/Makefile-Release.mk
@@ -35,8 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/_ext/1472/InitShader.o \
-	${OBJECTDIR}/_ext/1472/bose_udita_4.o
+	${OBJECTDIR}/_ext/5c0/InitShader.o \
+	${OBJECTDIR}/_ext/5c0/bose_udita_4.o
 
 
 # C Compiler Flags
@@ -63,15 +63,15 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/spherefinal: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/spherefinal ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/_ext/1472/InitShader.o: ../InitShader.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1472
+${OBJECTDIR}/_ext/5c0/InitShader.o: ../InitShader.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/InitShader.o ../InitShader.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/InitShader.o ../InitShader.cpp
 
-${OBJECTDIR}/_ext/1472/bose_udita_4.o: ../bose_udita_4.cpp 
-	${MKDIR} -p ${OBJECTDIR}/_ext/1472
+${OBJECTDIR}/_ext/5c0/bose_udita_4.o: ../bose_udita_4.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/bose_udita_4.o ../bose_udita_4.cpp
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/bose_udita_4.o ../bose_udita_4.cpp
 
 # Subprojects
 .build-subprojects:
@@ -79,7 +79,6 @@ ${OBJECTDIR}/_ext/1472/bose_udita_4.o: ../bose_udita_4.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/spherefinal
 
 # Subprojects
 .clean-subprojects:
--- WIP/src/learning/wip/WIP.java
+++ WIP/src/learning/wip/WIP.java
@@ -11,7 +11,7 @@ public class WIP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
